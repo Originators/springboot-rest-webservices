041951b6097970006550a0f379e585f24db3d932
Sunny Israni <sunnyisrani2893@gmail.com>
2020-01-12 10:46:33 +0530

    first commit

---
+++ src/test/java/com/restWebServices/SpringBoot2WebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2WebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
